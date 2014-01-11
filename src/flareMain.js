a28726c3f03d7226ca89694c5a3a84bfe94b439e
Katie Bryant <redkat231@gmail.com>
2014-01-11 01:16:13 +0000

    now drawing test squares
    git-svn-id: http://subversion.assembla.com/svn/projectflare/trunk@4 d7f86d05-3d01-4247-9a02-3338f5b2d2c1

--- src/flareMain.js
+++ src/flareMain.js
@@ -1,5 +1,7 @@
 //global variables
-
+var ctxWorld;
+var ctxDark;
+var ctxUI;
 
 function init() {
 	ctxWorld = document.getElementById('world').getContext('2d');
@@ -11,6 +13,17 @@ function init() {
 //main update loop, called at regular intervals
 function draw() {
 	//draw each canvas one at a time
+	ctxWorld.fillStyle = "purple";
+	ctxWorld.beginPath();
+	ctxWorld.rect(100, 100, 10, 10); 
+	ctxWorld.closePath();
+	ctxWorld.fill();
+	
+	ctxDark.fillStyle = "blue";
+	ctxDark.beginPath();
+	ctxDark.rect(105, 105, 20, 10); 
+	ctxDark.closePath();
+	ctxDark.fill();
 }
 
 window.addEventListener("load", init, false);
