d41b3637095e1091441a5ef1418a444a30dac4e5
Katie Bryant <redkat231@gmail.com>
2014-01-25 02:37:30 +0000

    SEXY RESIZING!!!
    git-svn-id: http://subversion.assembla.com/svn/projectflare/trunk@19 d7f86d05-3d01-4247-9a02-3338f5b2d2c1

--- src/flareMain.js
+++ src/flareMain.js
@@ -1,8 +1,50 @@
-var x = 10;
-var y = 10;
-var testImg;
+/*function loadAssets() {
+	//uncommenting this breaks things
+	//I will add the loading function here
+)*/
 
-//var mainGuy;
+function resizeScreen() {
+	//thanks to Gopherwood studios on html5rocks.com on how to do this
+	var screen = document.getElementById('gameScreen');
+	var gameAspectRatio = 4/3;
+	var newWidth = window.innerWidth;
+	var newHeight = window.innerHeight;
+	var windowAspectRatio = newWidth/newHeight;
+	
+	//adjust for windows not at the right aspect ratio:
+	if (windowAspectRatio > gameAspectRatio) {
+		newWidth = newHeight * gameAspectRatio;
+		screen.style.height = newHeight + 'px';
+		screen.style.width = newWidth + 'px';
+	} else { 
+		newHeight = newWidth / gameAspectRatio;
+		screen.style.width = newWidth + 'px';
+		screen.style.height = newHeight + 'px';
+	}
+	
+	//center the new screen:
+	screen.style.marginTop = (-newHeight / 2) + 'px';
+	screen.style.marginLeft = (-newWidth / 2) + 'px';
+	
+	//adjust canvas sizes
+	var worldC = document.getElementById('world');
+	worldC.width = newWidth;
+	worldC.height = newHeight;
+	var darkC = document.getElementById('dark');
+	darkC.width = newWidth;
+	darkC.height = newHeight;
+	var uiC = document.getElementById('ui');
+	darkC.width = newWidth;
+	darkC.height = newHeight;
+	
+	//set the global width and height:
+	GAME_WIDTH = newWidth;
+	GAME_HEIGHT = newHeight;
+	//set the unit size in pixels
+	//assuming one unit will be one tiles width
+	//6.6% is ~15 tiles
+	MEASURE_UNIT = Math.floor(newWidth * .066);
+}
 
 function initGame() {
 	//this is where we make sure the images and sounds have loaded, so we can safely use them!
@@ -10,9 +52,6 @@ function initGame() {
 	//generate the level
 	
 	//create a player instance
-	testImg = new Image();
-	testImg.src = "assets/testkitteh.png";
-		
     mainGuy =  new Player("assets/testkitteh.png" );
     
 }
@@ -20,7 +59,6 @@ function initGame() {
 //main update loop, called at regular intervals
 function draw() {
 	window.requestAnimationFrame(draw);
-	
 
 	//draw each canvas one at a time
 	// call the level draw method, which will draw the room and then all entities in it
@@ -28,50 +66,38 @@ function draw() {
 	
 	ctxWorld.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 	
+	//take out this block when we get tiles rendering
 	ctxWorld.fillStyle = "white";
 	ctxWorld.beginPath();
 	ctxWorld.rect(0, 0, GAME_WIDTH,GAME_HEIGHT); 
 	ctxWorld.closePath();
 	ctxWorld.fill();
-	//ctxWorld.drawImage(testImg, 600, 10);
-	
-	//directly draw darkness, accessing player position
-	//this is only an example
-	ctxDark.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 	 
-	 
-// !!!!!!!!!!!!!!!!!!!!!!!!!
-/*	
-	ctxDark.fillStyle = "blue";
-	ctxDark.beginPath();
-	ctxDark.rect(x, y, 50, 10); //(20, 10); 
-	ctxDark.closePath();
-	ctxDark.fill();
-
-	if (y<500) {
-		x++;
-		y++; 
-	}
-*/	// !!!!!!!!!!!!!!!!!!!! 
- 
- 
+	//the player should be drawn here, on top of the world
+	//player drawing and updates:
     mainGuy.draw(ctxWorld);
 
-	 mainGuy.update();  
+	mainGuy.update();
 
-    
+	
+	//directly draw darkness, accessing player position
+	ctxDark.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT); 
+
     
 	//directly draw the UI, asking for player resources with accessors
 	//no need for a subclass unless we want to animate the gauges
+	ctxUI.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT); 
+	
 }
 
 function initDrawUpdate() {
+	//size the window:
+	resizeScreen();
+	
 	//set up contexts:
 	ctxWorld = document.getElementById('world').getContext('2d');
 	ctxDark = document.getElementById('dark').getContext('2d');
 	ctxUI = document.getElementById('ui').getContext('2d');
-	   
-
 	
 	//set up gameplay elements
 	initGame();
@@ -82,6 +108,10 @@ function initDrawUpdate() {
 }
 
 window.addEventListener("load", initDrawUpdate, false);
+
+//account for user resizing the window
+window.addEventListener('resize', resizeScreen, false);
+window.addEventListener('orientationchange', resizeScreen, false);
 
 // for the movement control
 window.addEventListener('keyup', function(event) { controls.onKeyup(event); }, false);
