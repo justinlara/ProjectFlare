5b7d3d0e2a6ec397ba6754afcaac079b28a65bbe
Katie Bryant <redkat231@gmail.com>
2014-02-07 03:01:15 +0000

    added some simple rooms to play for when we have doors working
    git-svn-id: http://subversion.assembla.com/svn/projectflare/trunk@51 d7f86d05-3d01-4247-9a02-3338f5b2d2c1

--- src/allTiles.js
+++ src/allTiles.js
@@ -1,5 +1,5 @@
 function AllTiles() {
-	this.entrance = new Array();
+	this.entrance = new Array(); //empty room, for entrance only and for templating
 	this.entrance[0] = new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
 	this.entrance[1] = new Array(1,5,2,2,2,2,2,2,2,2,2,2,2,2,1);
 	this.entrance[2] = new Array(1,2,2,2,2,2,2,3,2,2,2,2,2,2,1);
@@ -12,10 +12,38 @@ function AllTiles() {
 	this.entrance[9] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
 	this.entrance[10] = new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
 	
+	this.r1 = new Array();//an open room with a lampe in the center, guarded by mileses
+	this.r1[0] = new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+	this.r1[1] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r1[2] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r1[3] = new Array(1,2,2,2,2,2,2,5,2,2,2,2,2,2,1);
+	this.r1[4] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r1[5] = new Array(1,2,2,2,2,5,2,3,2,5,2,2,2,2,1);
+	this.r1[6] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r1[7] = new Array(1,2,2,2,2,2,2,5,2,2,2,2,2,2,1);
+	this.r1[8] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r1[9] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r1[10] = new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
 	
-	/*this.getRandom() = function() {
-		//code for picking a random room goes here
-		//return 2D grid array
-		return this.entrance;
-	};*/
+	this.r2 = new Array(); // open room with lamp in center, 4miles in the corners
+	this.r2[0] = new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+	this.r2[1] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r2[2] = new Array(1,2,5,2,2,2,2,2,2,2,2,2,5,2,1);
+	this.r2[3] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r2[4] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r2[5] = new Array(1,2,2,2,2,2,2,3,2,2,2,2,2,2,1);
+	this.r2[6] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r2[7] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r2[8] = new Array(1,2,5,2,2,2,2,2,2,2,2,2,5,2,1);
+	this.r2[9] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r2[10] = new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+	
 }
+
+allTiles.prototype.getRandom() = function() {
+	//code for picking a random room goes here
+	//return 2D grid array
+	var r = Math.floor((Math.random()*2) + 1);
+	if (r == 1) return this.r1;
+	else if (r==2) return this.r2;
+};
