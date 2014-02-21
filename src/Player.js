2dd5eb664340fb6d406e74224b4c8242df56417d
Katie Bryant <redkat231@gmail.com>
2014-02-21 00:28:35 +0000

    player now entity being managed
    git-svn-id: http://subversion.assembla.com/svn/projectflare/trunk@73 d7f86d05-3d01-4247-9a02-3338f5b2d2c1

--- src/Player.js
+++ src/Player.js
@@ -57,105 +57,108 @@ function Player()
 				}
 	});
 
+	//Resize:
+	this.Resize = function()
+	{
+		var newfix = new b2FixtureDef; 
+ 
+		var resizeC = newfix.shape = new b2CircleShape; //b2PolygonShape; 
 	
-}
+		this.playerBoundBox.DestroyFixture( this.pFix ); //oldFix); 
+  
+		resizeC.SetRadius(((MEASURE_UNIT)/30)*(13/30));
+  
+		this.pFix = this.playerBoundBox.CreateFixture(newfix);
+	};
+	
+	//draw, overwrites entity draw
+	this.draw = function(w) 
+	{
+		this.update();
+	
+		var playerScale = 1;
+		var pw = MEASURE_UNIT * playerScale;
+		var ph = MEASURE_UNIT * playerScale;
+	
+		//Collision:
+		var c = this.playerfix.shape = new b2CircleShape; 
+		var r=c.GetRadius();
+		var offset =new  b2Vec2(Math.abs(centerX - this.p.pos[0]),  Math.abs(centerY  - this.p.pos[1]));  
+		var sx = offset.x; 
+		var sy = offset.y; 
+		this.p.playerBody.SetPosition(new b2Vec2( ((this.p.pos[0]+(.5*MEASURE_UNIT))/30), ((this.p.pos[1]+sy)/30) )); 
+	
+		//old draw:
+		//w.drawImage(this.p.I, this.p.pos[0], this.p.pos[1], pw, ph);
+	
+		//use sprite draw method
+		this.pSprite.draw(ctxWorld, this.p.pos[0], this.p.pos[1], pw, ph);
+  
+	};
+	
+	this.update = function() 
+	{
+		//if (controls.isDown(controls.UP)) this.moveUp();
+		//if (controls.isDown(controls.LEFT)) this.moveLeft();
+		//if (controls.isDown(controls.DOWN)) this.moveDown();
+		//if (controls.isDown(controls.RIGHT)) this.moveRight();
+  
+		if (controls.isDown(controls.UP)) this.moveUp();
+		else if (controls.isDown(controls.DOWN)) this.moveDown();
+	
+		if (controls.isDown(controls.LEFT)) this.moveLeft();
+		else if (controls.isDown(controls.RIGHT)) this.moveRight();
+  
+		if ('undefined' != typeof this.pSprite) {
+			if (!controls.isDown(controls.LEFT) &&
+				!controls.isDown(controls.RIGHT) &&
+				!controls.isDown(controls.UP) &&
+				!controls.isDown(controls.DOWN)){
+					this.pSprite.use('idle');
+				}
+		}
+	};
+
+	//movement now also rotates lantern
+	this.moveLeft = function()
+	{
+		arcStart = Math.PI*9/8;
+		arcEnd = Math.PI*7/8;
+		this.p.pos[0] -= MEASURE_UNIT*.07;
+		checkBounds(this.p);
+  
+	};
+
+	this.moveRight = function() 
+	{
+		arcStart = Math.PI*1/8;
+		arcEnd = Math.PI*15/8;
+		this.p.pos[0]  +=  MEASURE_UNIT*.07;
+		checkBounds(this.p);
+	};
+	this.moveUp = function() 
+	{
+		arcStart = Math.PI*13/8;
+		arcEnd = Math.PI*11/8;
+		this.p.pos[1] -=  MEASURE_UNIT*.07;
+		checkBounds(this.p);
+	};
+	this.moveDown = function()
+	{
+		arcStart = Math.PI*5/8;
+		arcEnd = Math.PI*3/8;
+		this.p.pos[1] +=  MEASURE_UNIT*.07;
+		this.pSprite.use('walkDown');
+		checkBounds(this.p);
+	};
+} //end constructor
+
 
 //Inheritance from entity class
 Player.prototype = new Entity;
 
-Player.prototype.Resize = function()
-{
-
-   var newfix = new b2FixtureDef; 
- 
-   var resizeC = newfix.shape = new b2CircleShape; //b2PolygonShape; 
-  
-  this.playerBoundBox.DestroyFixture( this.pFix ); //oldFix); 
-  
-   resizeC.SetRadius(((MEASURE_UNIT)/30)*(13/30));
-  
-  this.pFix = this.playerBoundBox.CreateFixture(newfix);  
-  
-};
-
-
-Player.prototype.draw = function(w) 
-{
-	this.update();
-
-  // -------- my method for drawing sprite ---------------	
-  /*
-  w.save();
-  w.translate(this.p.pos[0], this.p.pos[1]);
-
-  //this.p.sprite.draw(w);
-  w.restore();
-  */
-// -------- my method for drawing sprite END ---------------
-	
-	var playerScale = 1;
-	var pw = MEASURE_UNIT * playerScale;
-	var ph = MEASURE_UNIT * playerScale;
-	
-	//Collision:
-	var c = this.playerfix.shape = new b2CircleShape; 
-    var r=c.GetRadius();
-	var offset =new  b2Vec2(Math.abs(centerX - this.p.pos[0]),  Math.abs(centerY  - this.p.pos[1]));  
-	var sx = offset.x; 
-	var sy = offset.y; 
-	this.p.playerBody.SetPosition(new b2Vec2( ((this.p.pos[0]+(.5*MEASURE_UNIT))/30), ((this.p.pos[1]+sy)/30) )); 
-	
-	//old draw:
-	//w.drawImage(this.p.I, this.p.pos[0], this.p.pos[1], pw, ph);
-  
-	//use sprite draw method
-	this.pSprite.draw(ctxWorld, this.p.pos[0], this.p.pos[1], pw, ph);
-  
-};
-
-
-
-//movement now also rotates lantern
-Player.prototype.moveLeft = function()
- {
-  arcStart = Math.PI*9/8;
-  arcEnd = Math.PI*7/8;
-  this.p.pos[0] -= MEASURE_UNIT*.07;
-  checkBounds(this.p);
-  
-};
-
-Player.prototype.moveRight = function() 
-{
-  arcStart = Math.PI*1/8;
-  arcEnd = Math.PI*15/8;
-  this.p.pos[0]  +=  MEASURE_UNIT*.07;
-  checkBounds(this.p);
-
-  
-};
-
-Player.prototype.moveUp = function() 
-{
-  arcStart = Math.PI*13/8;
-  arcEnd = Math.PI*11/8;
-  this.p.pos[1] -=  MEASURE_UNIT*.07;
-  checkBounds(this.p);
-};
-
-Player.prototype.moveDown = function()
-{
-  arcStart = Math.PI*5/8;
-  arcEnd = Math.PI*3/8;
-  this.p.pos[1] +=  MEASURE_UNIT*.07;
-  this.pSprite.use('walkDown');
-  checkBounds(this.p);
-};
-
-function checkBounds(p)  
- {
-
-    
+checkBounds = function(p)  
+{    
     if(p.pos[0] < ((MEASURE_UNIT* 22)/30))
     {
         p.pos[0] = ((MEASURE_UNIT*22)/30); 
@@ -177,29 +180,4 @@ function checkBounds(p)
         p.pos[1] = ((Math.abs(GAME_HEIGHT/30 - (9.4* MEASURE_UNIT))/30)*30);
     }
  
-}
-
-
-
-
-Player.prototype.update = function() 
-{
-  //if (controls.isDown(controls.UP)) this.moveUp();
-  //if (controls.isDown(controls.LEFT)) this.moveLeft();
-  //if (controls.isDown(controls.DOWN)) this.moveDown();
-  //if (controls.isDown(controls.RIGHT)) this.moveRight();
-  
-  if (controls.isDown(controls.UP)) this.moveUp();
-  else if (controls.isDown(controls.DOWN)) this.moveDown();
-  
-  if (controls.isDown(controls.LEFT)) this.moveLeft();
-  else if (controls.isDown(controls.RIGHT)) this.moveRight();
-  
-  if ('undefined' != typeof this.pSprite) {
-  if (!controls.isDown(controls.LEFT) &&
-		!controls.isDown(controls.RIGHT) &&
-		!controls.isDown(controls.UP) &&
-		!controls.isDown(controls.DOWN)){
-	this.pSprite.use('idle');
-  }}
 };
