a7def0bccd0dc5de6afb60b4d72620a8f034e2ac
Katie Bryant <redkat231@gmail.com>
2014-04-15 23:30:22 +0000

    derp forgot to add this
    git-svn-id: http://subversion.assembla.com/svn/projectflare/trunk@169 d7f86d05-3d01-4247-9a02-3338f5b2d2c1

---
+++ src/lightEffects.js
@@ -0,0 +1,26 @@
+//file for temporary effects that draw on the dark canvas
+
+var lightlampEffect = function(lampX, lampY) {
+	//draw an expanding circle of black, xor composite
+	var lx = lampX + MEASURE_UNIT/2;
+	var ly = lampY + MEASURE_UNIT/2;
+	var r = MEASURE_UNIT;
+	var e = setInterval(function() {
+		ctxDark.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+		ctxDark.globalCompositeOperation = 'source-over';
+		ctxDark.globalAlpha = 1;
+		ctxDark.fillStyle = 'black';
+		ctxDark.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+		ctxDark.globalCompositeOperation = 'xor';
+		ctxDark.beginPath();
+		ctxDark.arc(200,200,50, 0, Math.PI*2);
+		ctxDark.fill();
+		r+=10;
+		console.log("lighttest");
+	}, 30);
+	setTimeout(clearInterval(e), 1000);
+};
+
+var lightning = function() {
+
+};
