18fb925235f4fc1b403b6586acb84cfbab4f07a0
Katie Bryant <redkat231@gmail.com>
2014-04-15 01:29:57 +0000

    miles is wanderer.  munge is chaser
    git-svn-id: http://subversion.assembla.com/svn/projectflare/trunk@155 d7f86d05-3d01-4247-9a02-3338f5b2d2c1

--- src/enemyMiles.js
+++ src/enemyMiles.js
@@ -13,7 +13,7 @@ function Miles() {
 	this.escapeSpeed = MEASURE_UNIT * .02;
 	this.attackRange = MEASURE_UNIT * 0;
 	
-	this.enemyBehavior = new Behavior(this, 'wander', 'follow', 'none', 'none', 'none', 'none');
+	this.enemyBehavior = new Behavior(this, 'wander', 'none', 'none', 'none', 'none', 'none');
 }
 
 Miles.prototype = Object.create(Enemy.prototype);
