43bcaf6970fa48f570f50446b2686a0731a70adb
Katie Bryant <redkat231@gmail.com>
2014-04-17 00:49:29 +0000

    made big open rooms with few enemies less open
    git-svn-id: http://subversion.assembla.com/svn/projectflare/trunk@174 d7f86d05-3d01-4247-9a02-3338f5b2d2c1

--- src/allTiles.js
+++ src/allTiles.js
@@ -27,27 +27,27 @@ function AllTiles() {
 	this.r1 = new Array();//an open room with a lamp in the center, guarded by mileses
 	this.r1[0] = new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
 	this.r1[1] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
-	this.r1[2] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r1[2] = new Array(1,2,2,4,2,2,2,2,2,2,2,4,2,2,1);
 	this.r1[3] = new Array(1,2,2,2,2,2,2,5,2,2,2,2,2,2,1);
 	this.r1[4] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
 	this.r1[5] = new Array(1,2,2,2,2,5,2,3,2,5,2,2,2,2,1);
-	this.r1[6] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
-	this.r1[7] = new Array(1,2,2,2,2,2,2,5,2,2,2,2,2,2,1);
+	this.r1[6] = new Array(1,2,2,2,2,4,2,2,2,2,2,2,2,2,1);
+	this.r1[7] = new Array(1,2,2,4,2,2,2,5,2,2,2,4,2,2,1);
 	this.r1[8] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
 	this.r1[9] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
 	this.r1[10] = new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
 	
 	this.r2 = new Array(); // open room with lamp in center, 4miles in the corners
 	this.r2[0] = new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
-	this.r2[1] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
-	this.r2[2] = new Array(1,2,5,2,2,2,2,2,2,2,2,2,5,2,1);
-	this.r2[3] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
-	this.r2[4] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r2[1] = new Array(1,2,4,2,2,2,2,2,2,2,2,2,4,2,1);
+	this.r2[2] = new Array(1,4,5,2,2,2,2,2,2,2,2,2,5,4,1);
+	this.r2[3] = new Array(1,2,2,2,4,4,2,2,2,4,4,2,2,2,1);
+	this.r2[4] = new Array(1,2,2,2,4,2,2,2,2,2,4,2,2,2,1);
 	this.r2[5] = new Array(1,2,2,2,2,2,2,3,2,2,2,2,2,2,1);
 	this.r2[6] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
-	this.r2[7] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
-	this.r2[8] = new Array(1,2,5,2,2,2,2,2,2,2,2,2,5,2,1);
-	this.r2[9] = new Array(1,2,2,2,2,2,2,2,2,2,2,2,2,2,1);
+	this.r2[7] = new Array(1,2,2,2,4,4,2,2,2,4,4,2,2,2,1);
+	this.r2[8] = new Array(1,4,5,2,2,2,2,2,2,2,2,2,5,4,1);
+	this.r2[9] = new Array(1,2,4,2,2,2,2,2,2,2,2,2,4,2,1);
 	this.r2[10] = new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
 	
 	this.r3 = new Array(); //3 miles maze
