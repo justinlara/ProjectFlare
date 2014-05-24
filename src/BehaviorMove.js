8e6f9db41e2f642b97c901e955a24b9ea7fe891c
Chris Noel <cnoel@uci.edu>
2014-05-24 03:21:30 +0000

    Orthogonal movement added.
    git-svn-id: http://subversion.assembla.com/svn/projectflare/trunk@317 d7f86d05-3d01-4247-9a02-3338f5b2d2c1

--- src/BehaviorMove.js
+++ src/BehaviorMove.js
@@ -17,10 +17,31 @@ var MoveBehavior = function(){
 			actor.newTarget();
 		}
 	};
+	
+	this.ortho = function(actor) {
+		
+		if(Math.floor(actor.positions.pos[0]) < Math.floor(actor.targetPosX)){
+			actor.xdelta += actor.speed;
+		} else {
+			actor.xdelta -= actor.speed;
+		}
+		else if(Math.floor(actor.positions.pos[1]) < Math.floor(actor.targetPosY)){
+			actor.ydelta += actor.speed;
+		} else {
+			actor.ydelta -= actor.speed;
+		}
+		
+		if( actor.entityBehavior.distanceToTarget() < (MEASURE_UNIT/2)) {
+			actor.newTarget();
+		}
+	};
 
 	this.move = function(movetype, actor) {
 		if(movetype == "wander") {
 			this.wander(actor);
+		}
+		else if(movetype == "ortho") {
+			this.ortho(actor);
 		}
 		
 		else {}  //if any invalid type is given, don't act
